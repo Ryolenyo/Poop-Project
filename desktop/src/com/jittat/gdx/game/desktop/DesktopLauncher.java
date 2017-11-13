e78174e5a7a3ddb3b61190f041ceededeb1d03ea
Ryolenyo <ryolenyo@gmail.com>
2017-11-13 21:47:44 +0700

    Add background

--- desktop/src/com/jittat/gdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/jittat/gdx/game/desktop/DesktopLauncher.java
@@ -2,13 +2,13 @@ package com.jittat.gdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.jittat.gdx.game.Poop;
+import com.jittat.gdx.game.PoopScreen;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1000;
 		config.height = 750;
-		new LwjglApplication(new Poop(), config);
+		new LwjglApplication(new PoopScreen(), config);
 	}
 }
