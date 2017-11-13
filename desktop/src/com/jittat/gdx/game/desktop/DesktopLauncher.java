2babbb76810b6a9603f79a002a83da9fc8fd853d
Ryolenyo <ryolenyo@gmail.com>
2017-11-13 20:55:31 +0700

    Upload file

---
+++ desktop/src/com/jittat/gdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.jittat.gdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jittat.gdx.game.Poop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1000;
+		config.height = 750;
+		new LwjglApplication(new Poop(), config);
+	}
+}
