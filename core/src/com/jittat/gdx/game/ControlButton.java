70d1c5d611e8ab8fbb16096bbab024a654823282
Ryolenyo <ryolenyo@gmail.com>
2017-12-17 02:13:57 +0700

    detect wasd

--- core/src/com/jittat/gdx/game/ControlButton.java
+++ core/src/com/jittat/gdx/game/ControlButton.java
@@ -5,19 +5,12 @@ import java.util.Random;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ControlButton {
-	Button button = null;
-	SpriteBatch batch;
 
-	public void randomInt(int state){
-		
-		Random rand = new Random();
-		int[] arrayNum = new int[state];
-		
-		for (int x = 0 ; x < state ; x++) {
-			arrayNum[x] = rand.nextInt(10);
-		}
-		
-	}
-	
+		public void randomInt(){
+			
+			int n = 1 ;
+			n = n+1;		
+			
+		}		
 
 }
--- core/src/com/jittat/gdx/game/PoopScreen.java
+++ core/src/com/jittat/gdx/game/PoopScreen.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class PoopScreen extends ApplicationAdapter {
 	Gauge guage;
@@ -20,12 +22,31 @@ public class PoopScreen extends ApplicationAdapter {
 	int state = 0;
 	boolean count = false;
 	
-	public void update() {
+	public void update(String x) {
+		boolean unstop = true;
+		int key = 0;
+		if (x == "w") {
+			key = Keys.W;
+		}
+		else if (x == "a") {
+			key = Keys.A;
+		}
+		else if (x == "s") {
+			key = Keys.S;
+		}
+		else if (x == "d") {
+			key = Keys.D;
+		}
 		
-		if (Gdx.input.isKeyPressed(Keys.UP)) {
+		if (state==10)
+		{
+			unstop = false;
+		}
+		
+		if (Gdx.input.isKeyPressed(key) & unstop) {
 			count = true;
 		}
-		if (count & Gdx.input.isKeyPressed(Keys.UP)==false)
+		if (count & Gdx.input.isKeyPressed(key)==false & unstop)
 		{
 			state += 1;
 			count = false;
@@ -42,23 +63,63 @@ public class PoopScreen extends ApplicationAdapter {
 				
 	}
 
-
+	public void control(int state) {
+		int n = 0;
+		for (int i = 0 ; i < state+1 ; i++)
+		{
+			Random rand = new Random();
+			n = rand.nextInt(4);
+			choose(n);
+			
+		///////////////DELAY///////////////
+//		try {
+//			TimeUnit.SECONDS.sleep(1);
+///		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		///////////////////////////////////
+			
+//			System.out.println(n);
+		}
+	}
+	
+	
+	public void choose(int toggle) {
+		
+		if (toggle == 0) {
+			batch.draw(button.buttonBox(0,1),220,120);
+		}
+		else if (toggle == 1){
+			batch.draw(button.buttonBox(1,1),150,50);
+		}
+		else if (toggle == 2){
+			batch.draw(button.buttonBox(2,1),220,50);
+		}
+		else if (toggle == 3){
+			batch.draw(button.buttonBox(3,1),290,50);
+		}
+	}
+	
 
 	public void render () {
+		
 		batch.begin();
-		update();
 		
 		batch.draw(background, 0, 0);
 		
+		///////////////BUTTON///////////////
+				
 		batch.draw(button.buttonBox(0,0),220,120);
 		batch.draw(button.buttonBox(1,0),150,50);
 		batch.draw(button.buttonBox(2,0),220,50);
 		batch.draw(button.buttonBox(3,0),290,50);
 		
+		control(state); //show sequence with red button
+
+		
 		batch.draw(gauge.callGauge(state), 50,200);
 		
-//		controlButton.randomInt(0);
-	
 		batch.end();
 	}
 	
